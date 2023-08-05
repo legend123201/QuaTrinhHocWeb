62065b4f47728b43d0a37928c42f2acf489c5d0f
Louis Tran <legendofall18@gmail.com>
2023-08-05 20:30:34 +0700

    Add file forinforof and optionalChaining in the JSCore folder

---
+++ src/JSCore/ForInForOf.js
@@ -0,0 +1,3 @@
+// for in dành cho object, lưu ý là nó lặp qua key của object chứ ko phải value, nên nếu dùng cho array thì mỗi phần tử là index
+// for of dành cho array
+// => cách nhớ dễ, for Of thì Of có chữ O nhưng ko phải là object
---
+++ src/JSCore/OptionalChaining.js
@@ -0,0 +1,5 @@
+// là dấu "?."
+// chỉ hỗ trợ từ node 13 hoặc 14 đổ lên
+
+// ví dụ:
+// oldDataItem.device?.[0]?.site;
