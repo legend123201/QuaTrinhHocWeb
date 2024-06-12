7667ed4ab2014d82b1545ee34ca04f1c25e01097
Louis Tran <legendofall18@gmail.com>
2024-06-12 22:42:41 +0700

    Update the timeLog.js file

--- src/JSCore/timeLog.js
+++ src/JSCore/timeLog.js
@@ -1,6 +1,12 @@
+/*
+    console.time(X) => start the time with the label X, not log the time to the terminal console
+    console.timeLog(X) => log the time from the beginning of the label X to the current time to the terminal console
+    console.timeEnd(X) => stop counting the time for the label X and log the current time to the terminal console
+*/
+
 const test = () => {
-  console.time("answer time");
-  console.time("answer time 1");
+  console.time("check");
+  console.timeLog("check");
 
   let a = 0;
 
@@ -8,16 +14,13 @@ const test = () => {
     a = a + 1;
   }
 
-  console.timeLog("answer time");
-  console.timeEnd("answer time 1");
-  console.time("answer time 2");
+  console.timeLog("check");
 
   for (let i = 0; i < 100000000; i++) {
     a = a + 1;
   }
 
-  console.timeEnd("answer time");
-  console.timeEnd("answer time 2");
+  console.timeEnd("check");
 };
 
 test();
